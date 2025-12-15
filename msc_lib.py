a526e4be92443d03d0a1cc1490b8fe795fe599b6
TangXudong1207 <txd20211207@gmail.com>
2025-12-16 05:28:10 +0800

    Update msc_lib.py

--- msc_lib.py
+++ msc_lib.py
@@ -38,7 +38,7 @@ def init_system():
             vertexai.init(project=creds_dict['project_id'], location='us-central1', credentials=creds)
             
             # 加载 Gemini Pro
-            vertex_gemini = GenerativeModel("gemini-3-pro-preview") 
+            vertex_gemini = GenerativeModel("gemini-1.5-pro-002") 
             # 加载 Gecko
             vertex_embed = TextEmbeddingModel.from_pretrained("text-embedding-004")
     except Exception as e:
